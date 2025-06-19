e6a68e3f21a924612bb2c9bfd3d3a808de75e537
SamuelAlves903 <alvessamuel205@gmail.com>
2025-06-18 22:43:52 -0300

    Usuario Login

--- Java/src/main/java/org/example/java/JavaApplication.java
+++ Java/src/main/java/org/example/java/JavaApplication.java
@@ -1,15 +1,23 @@
 package org.example.java;
 
+import org.example.java.model.Usuario;
+import org.example.java.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class JavaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(JavaApplication.class, args);}
+    @Bean
+    public CommandLineRunner initDataBase(UsuarioRepository usuarioRepository) {
+        return args -> {
+
+            usuarioRepository.save(new Usuario(null, "admin", new BCryptPasswordEncoder().encode("admin"), "Admin", "ADMIN"));
+            usuarioRepository.save(new Usuario(null, "user", new BCryptPasswordEncoder().encode("user"), "user", "USER"));
+        };
     }
-
-
-
 }
---
+++ Java/src/main/java/org/example/java/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package org.example.java.repository;
+
+import org.example.java.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
