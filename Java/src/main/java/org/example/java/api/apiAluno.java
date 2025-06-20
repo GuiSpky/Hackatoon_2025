cd92740ba450dda0898d9227f9868457146f60b4
Guilherme Fernandes de Souza <guii0410@gmail.com>
2025-06-20 15:22:37 -0300

    criado api aluno e turma

---
+++ Java/src/main/java/org/example/java/api/apiAluno.java
@@ -0,0 +1,43 @@
+package org.example.java.api;
+
+import lombok.AllArgsConstructor;
+import org.example.java.model.Aluno;
+import org.example.java.model.Turma;
+import org.example.java.service.AlunoService;
+import org.example.java.service.TurmaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/Aluno")
+@AllArgsConstructor
+public class apiAluno {
+
+    private final AlunoService service;
+
+
+    @GetMapping()
+    public ResponseEntity<List<Aluno>> listar() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @PostMapping()
+    public ResponseEntity salvar(@RequestBody Aluno aluno) {
+        service.salvar(aluno);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping()
+    public ResponseEntity alterar(@RequestBody Aluno aluno) {
+        service.salvar(aluno);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deletar(@PathVariable(name = "id") Long id) {
+        service.deletarPorId(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ Java/src/main/java/org/example/java/api/apiTurma.java
@@ -0,0 +1,42 @@
+package org.example.java.api;
+
+import lombok.AllArgsConstructor;
+import org.example.java.model.Turma;
+import org.example.java.service.TurmaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/Turma")
+@AllArgsConstructor
+public class apiTurma {
+
+
+    private final TurmaService service;
+
+
+    @GetMapping()
+    public ResponseEntity<List<Turma>> listar() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @PostMapping()
+    public ResponseEntity salvar(@RequestBody Turma turma) {
+        service.salvar(turma);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping()
+    public ResponseEntity alterar(@RequestBody Turma turma) {
+        service.salvar(turma);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deletar(@PathVariable(name = "id") Long id) {
+        service.deletarPorId(id);
+        return ResponseEntity.ok().build();
+    }
+}
