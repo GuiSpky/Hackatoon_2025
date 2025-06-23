5081892a3a53f792eae0f18b10a56d9fc3144ec8
Guilherme Fernandes de Souza <guii0410@gmail.com>
2025-06-23 20:18:15 -0300

    comeco do gabarito

--- Java/src/main/java/org/example/java/api/apiProva.java
+++ Java/src/main/java/org/example/java/api/apiProva.java
@@ -2,35 +2,35 @@ package org.example.java.api;
 
 import lombok.AllArgsConstructor;
 import org.example.java.model.Aluno;
-import org.example.java.model.Turma;
+import org.example.java.model.Prova;
 import org.example.java.service.AlunoService;
-import org.example.java.service.TurmaService;
+import org.example.java.service.ProvaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/Aluno")
+@RequestMapping("api/Prova")
 @AllArgsConstructor
-public class apiAluno {
+public class apiProva {
 
-    private final AlunoService service;
+    private final ProvaService service;
 
     @GetMapping()
-    public ResponseEntity<List<Aluno>> listar() {
+    public ResponseEntity<List<Prova>> listar() {
         return ResponseEntity.ok(service.listarTodos());
     }
 
     @PostMapping()
-    public ResponseEntity salvar(@RequestBody Aluno aluno) {
-        service.salvar(aluno);
+    public ResponseEntity salvar(@RequestBody Prova prova) {
+        service.salvar(prova);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping()
-    public ResponseEntity alterar(@RequestBody Aluno aluno) {
-        service.salvar(aluno);
+    public ResponseEntity alterar(@RequestBody Prova prova) {
+        service.salvar(prova);
         return ResponseEntity.ok().build();
     }
 
---
+++ Java/src/main/java/org/example/java/model/CorrecaoProva.java
@@ -0,0 +1,29 @@
+package org.example.java.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CorrecaoProva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    private Prova prova;
+
+
+
+    private Float valor;
+    private String gabarito;
+    private String resposta;
+}
